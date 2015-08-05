f7d732d3730c72a088ddb28dbdf5dfc2e513636f
Jason Villalon <jasonvillalon@gmail.com>
2015-08-05 12:08:56 +0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,190 @@
+export default {
+  scrollY: {
+    "data-scroll-y": ""
+  },
+  scrollX: {
+    "data-scroll-x": ""
+  },
+  layout: (layout) => {
+    return {
+      "data-layout": layout ? layout : ""
+    }
+  },
+  layoutSM: (layout) => {
+    return {
+      "data-layout-sm": layout ? layout : ""
+    }
+  },
+  layoutGTSM: (layout) => {
+    return {
+      "data-layout-gt-sm": layout ? layout : ""
+    }
+  },
+  layoutMD: (layout) => {
+    return {
+      "data-layout-md": layout ? layout : ""
+    }
+  },
+  layoutGTMD: (layout) => {
+    return {
+      "data-layout-gt-md": layout ? layout : ""
+    }
+  },
+  layoutLG: (layout) => {
+    return {
+      "data-layout-lg": layout ? layout : ""
+    }
+  },
+  layoutGTLG: (layout) => {
+    return {
+      "data-layout-gt-lg": layout ? layout : ""
+    }
+  },
+  flex: (number) => {
+    return {
+      "data-flex": number ? number : ""
+    }
+  },
+  flexSM: (number) => {
+    return {
+      "data-flex-sm": number ? number : ""
+    }
+  },
+  flexLTMD: (number) => {
+    return {
+      "data-flex-lt-md": number ? number : ""
+    }
+  },
+  flexMD: (number) => {
+    return {
+      "data-flex-md": number ? number : ""
+    }
+  },
+  flexLTLG: (number) => {
+    return {
+      "data-flex-lt-lg": number ? number : ""
+    }
+  },
+  flexLG: (number) => {
+    return {
+      "data-flex-lg": number ? number : ""
+    }
+  },
+  margin: {
+    "data-layout-margin": ""
+  },
+  padding: {
+    "data-layout-padding": ""
+  },
+  wrap: {
+    "data-layout-wrap": ""
+  },
+  fill: {
+    "data-layout-fill": ""
+  },
+  hideAll: (bool) => {
+    return {"data-hide": bool}
+  },
+  hideSM: (bool) => {
+    return {"data-hide-sm": bool}
+  },
+  hideGTSM: (bool) => {
+    return {"data-hide-gt-sm": bool}
+  },
+  hideMD: (bool) => {
+    return {"data-hide-md": bool}
+  },
+  hideGTMD: (bool) => {
+    return {"data-hide-gt-md": bool}
+  },
+  hideLG: (bool) => {
+    return {"data-hide-lg": bool}
+  },
+  hideGTLG: (bool) => {
+    return {"data-hide-gt-lg": bool}
+  },
+  showAll: (bool) => {
+    return {"data-show": bool}
+  },
+  showSM: (bool) => {
+    return {"data-show-sm": bool}
+  },
+  showGTSM: (bool) => {
+    return {"data-show-gt-sm": bool}
+  },
+  showMD: (bool) => {
+    return {"data-show-md": bool}
+  },
+  showGTMD: (bool) => {
+    return {"data-show-gt-md": bool}
+  },
+  showLG: (bool) => {
+    return {"data-show-lg": bool}
+  },
+  showGTLG: (bool) => {
+    return {"data-show-gt-lg": bool}
+  },
+  layoutAlign: (align) => {
+    return {"data-layout-align": align}
+  },
+  layoutAlignSM: (align) => {
+    return {"data-layout-align-sm": align}
+  },
+  layoutAlignGTSM: (align) => {
+    return {"data-layout-align-gt-sm": align}
+  },
+  layoutAlignMD: (align) => {
+    return {"data-layout-align-md": align}
+  },
+  layoutAlignGTMD: (align) => {
+    return {"data-layout-align-gt-md": align}
+  },
+  layoutAlignLG: (align) => {
+    return {"data-layout-align-lg": align}
+  },
+  layoutAlignGTLG: (align) => {
+    return {"data-layout-align-gt-lg": align}
+  },
+  offset: (value) => {
+    return {"data-offset": value}
+  },
+  offsetSM: (value) => {
+    return {"data-offset-sm": value}
+  },
+  offsetGTSM: (value) => {
+    return {"data-offset-gt-sm": value}
+  },
+  offsetMD: (value) => {
+    return {"data-offset-md": value}
+  },
+  offsetGTMD: (value) => {
+    return {"data-offset-gt-md": value}
+  },
+  offsetLG: (value) => {
+    return {"data-offset-lg": value}
+  },
+  offsetGTLG: (value) => {
+    return {"data-offset-gt-lg": value}
+  },
+  flexOrder: (value) => {
+    return {"data-flex-order": value}
+  },
+  flexOrderSM: (value) => {
+    return {"data-flex-order-sm": value}
+  },
+  flexOrderGTSM: (value) => {
+    return {"data-flex-order-gt-sm": value}
+  },
+  flexOrderMD: (value) => {
+    return {"data-flex-order-md": value}
+  },
+  flexOrderGTMD: (value) => {
+    return {"data-flex-order-gt-md": value}
+  },
+  flexOrderLG: (value) => {
+    return {"data-flex-order-lg": value}
+  },
+  flexOrderGTLG: (value) => {
+    return {"data-flex-order-gt-lg": value}
+  },
+}
